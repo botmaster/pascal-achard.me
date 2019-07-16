0dcc4628d5c0d11b6dccc5506d983cd15c8ceae7
Pascal Achard <pascal@lahautesoceiete.com>
2019-07-16 15:57:12 +0200

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
     ** Headers of the page
     */
     head: {
+        // this htmlAttrs you need
+        htmlAttrs: {
+            lang: 'fr-fr',
+        },
         title: process.env.npm_package_name || '',
         meta: [
             { charset: 'utf-8' },
