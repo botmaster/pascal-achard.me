62cd4a6323510d4bf443807c8ce6aec2eb12c1fa
botmaster <pascal.achard@gmail.com>
2019-07-22 11:51:35 +0200

    Update hero background image

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
     <main class="page-index">
         <div class="section page-index__cover cover min-h-screen flex items-end relative py-6 md:py-32">
-            <div class="cover__background absolute inset-0"></div>
+            <img src="../assets/images/pascal-achard.jpg" class="cover__background absolute inset-0 w-full h-full"
+                 alt="Pascal Achard Senior Frontend developer">
             <div class="cover__dimmer absolute inset-0"></div>
             <div class="container mx-auto px-container md:px-container-md relative">
                 <div>
@@ -19,7 +20,7 @@
             <div class="container mx-auto px-container md:px-container-md">
                 <div class="-mx-gutter md:-mx-gutter-md flex">
                     <div class="md:w-8/12 px-gutter md:px-gutter-md">
-                        <h4 class="text-gray-900">À propos de moi</h4>
+                        <h3 class="h4 text-gray-900">À propos de moi</h3>
                         <p class="text-lg mt-6 text-gray-800">
                             Je suis développeur front-end sénior depuis 3 ans mais j'ai 20 ans d'expérience dans
                             la création de sites Web et d'applications Web. Je me suis spécialisé dans le JavaScript et
@@ -42,7 +43,7 @@
                 </div>
                 <div class="-mx-gutter md:-mx-gutter-md flex">
                     <div class=" md:w-8/12 px-gutter md:px-gutter-md">
-                        <h4 class="text-gray-900 mt-16">En ce moment je suis fan de</h4>
+                        <h3 class="h4 text-gray-900 mt-16">En ce moment je suis fan de</h3>
                         <p class="text-gray-800 mt-3">
                             Vue.js, Vuex, Craftcms, Twig, tailwindcss, GraphQL, Docker, Webpack, Prettier, Nuxt.js.
                         </p>
@@ -70,10 +71,12 @@
 
     .cover {
         &__background {
-            background-image: url("~assets/images/pascal-achard.jpg");
-            background-size: cover;
-            background-position-x: center;
-            background-position-y: center;
+            /*background-image: url("~assets/images/pascal-achard.jpg");*/
+            /*background-size: cover;*/
+            /*background-position-x: center;*/
+            /*background-position-y: center;*/
+            display: block;
+            object-fit: cover;
         }
 
         &__dimmer {
