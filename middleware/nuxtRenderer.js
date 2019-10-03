5081dbb4382e1c2182e0847aaf33ab34762c378e
botmaster <pascal.achard@gmail.com>
2019-10-03 11:00:16 +0200

    Fix NuxtRender Component
    Fix test static mode

--- middleware/nuxtRenderer.js
+++ middleware/nuxtRenderer.js
@@ -1,7 +1,8 @@
 export default function({ store }) {
     console.log("process.server ", process.server);
+    console.log("process.static ", process.static);
     if (store.state.rss === process.server && !process.static) {
         return;
     }
-    store.commit("SET_RENDERER", process.server);
+    store.commit("SET_RENDERER", process.server && !process.static);
 }
