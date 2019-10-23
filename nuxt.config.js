2a28a233361c75fa6db34219be4aaebcd0372705
botmaster <pascal.achard@gmail.com>
2019-10-23 15:40:49 +0200

    Update favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,9 +35,45 @@ export default {
             {
                 property: "twitter:image",
                 content: "https://www.pascal-achard.me/social-fb.jpg"
+            },
+            {
+                name: "msapplication-TileColor",
+                content: "#da532c"
+            },
+            {
+                name: "theme-color",
+                content: "#ffffff"
             }
         ],
-        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+        link: [
+            {
+                rel: "apple-touch-icon",
+                sizes: "180x180",
+                href: "/apple-touch-icon.png"
+            },
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "32x32",
+                href: "/favicon-32x32.png"
+            },
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "16x16",
+                href: "/favicon-16x16.png"
+            },
+            {
+                rel: "manifest",
+                href: "/site.webmanifest"
+            },
+            {
+                rel: "mask-icon",
+                href: "/safari-pinned-tab.svg",
+                color: "#5bbad5"
+            },
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+        ]
     },
     /*
      ** Customize the progress-bar color
